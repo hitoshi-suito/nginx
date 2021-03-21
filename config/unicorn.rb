73a4d1eea40f2f4a174a04397492c0ecc42a7778
hitoshi-suito <htshts153@gmail.com>
2021-03-21 17:28:37 +0900

    init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,9 @@
-worker_processes 2
-working_directory "/Users/hitoshi/workspace/dic/nginx_exercise/blog_app" #appと同じ階層を指定
-
-timeout 3600
-
-
-listen "/var/run/unicorn//Users/hitoshi/workspace/dic/nginx_exercise/blog_app/app.sock" ★
-pid "/var/run/unicorn//Users/hitoshi/workspace/dic/nginx_exercise/blog_app/app.pid"
-
-
-stderr_path "/home/anime/deploy_sample/current/log/unicorn.log"
-stdout_path "/home/anime/deploy_sample/current/log/unicorn.log"
-
-
+require 'fileutils'
 preload_app true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
+
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
+end
